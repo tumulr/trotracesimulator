89d2ef3de39e3ce5364ef8099431ea7726817f37
Tumul Richesh <tumullko@gmail.com>
2021-08-30 20:37:09 +0530

    Update worker.js

--- race_simulator/app/controllers/worker.js
+++ race_simulator/app/controllers/worker.js
@@ -1,10 +1,22 @@
 'use strict';
 
 const Worker = require('../classes/worker');
+const Horse = require('../classes/horse');
 
 class WorkerController {
   constructor() {
     this.worker = new Worker();
+    this.horse = new Horse();
+  }
+
+  async fillData(req, res) {
+    try {
+      await this.horse.fillData();
+
+      res.status(200).json({message: 'SUCCESS'});
+    } catch(err) {
+      res.status(400).json({message: err.message});
+    }
   }
 
   async fetchActiveRaces(req, res) {
